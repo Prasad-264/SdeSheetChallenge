a5b45083493bc2da89399186c53dc3e097ea5a04
Prasad Kandekar <prasadkandekar555@gmail.com>
2022-06-09 18:04:59 +0530

    search in 2D matrix

--- Day5_Array/search_a_2D_matrix.cpp
+++ Day5_Array/search_a_2D_matrix.cpp
@@ -57,3 +57,49 @@ public:
 
 //* TC = O(log(m*n))
 //* SC = O(1)
+
+// GFG problem
+/*
+Given a matrix mat[][] of size N x M, where every row and column is sorted in increasing order, 
+and a number X is given. The task is to find whether element X is present in the matrix or not.
+
+Example 1:
+
+Input:
+N = 3, M = 3
+mat[][] = 3 30 38 
+         44 52 54 
+         57 60 69
+X = 62
+Output: 0
+Explanation:
+62 is not present in the
+matrix, so output is 0
+
+Example 2:
+
+Input:
+N = 1, M = 6
+mat[][] = 18 21 27 38 55 67
+X = 55
+Output: 1
+Explanation:
+55 is present in the
+matrix at 5th cell.
+*/
+
+class Solution {
+public:	
+	int matSearch(vector<vector<int>>& mat, int n, int m, int x) {
+	    int i = 0, j = m - 1;
+	    while(i < n && j >= 0) {
+	        if(mat[i][j] == x) return 1;
+	        else if(mat[i][j] > x) j--;
+	        else i++;
+	    }
+	    return 0;
+	}
+};
+
+//* TC = O(log(m*n))
+//* SC = O(1)
