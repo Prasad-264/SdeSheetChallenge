d056f38f5c37bc6b9225ccf4b4e2e8b85b73f9c0
Prasad Kandekar <prasadkandekar555@gmail.com>
2022-06-09 20:27:15 +0530

    commited

--- Day5_Array/majority_element_II.cpp
+++ Day5_Array/majority_element_II.cpp
@@ -17,3 +17,50 @@ Input: nums = [1,2]
 Output: [1,2]
 */
 
+//* Hashing (Maping)
+
+vector<int> majorityElement(int arr[], int n) {
+    unordered_map < int, int > mp;
+    vector < int > ans;
+
+    for (int i = 0; i < n; i++) {
+        mp[arr[i]]++;
+    }
+
+    for (auto x: mp) {
+        if (x.second > (n / 3))
+        ans.push_back(x.first);
+    }
+    return ans;
+}
+
+// TC = O(N)
+// SC = O(N)
+
+
+//* Optimal Solution (Extended Boyer Moore’s Voting Algorithm)
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& a) {
+        int y = -1, z = -1, cy = 0, cz = 0;
+        for (const auto & x: a)  {
+		    if (x == y) cy++;
+		    else if (x == z) cz++;
+		    else if (! cy) y = x, cy = 1;
+		    else if (! cz) z = x, cz = 1;
+		    else cy--, cz--;
+        }      
+        cy = cz = 0;
+        for (const auto & x: a)
+    		 if (x == y) cy++;
+			 else if (x == z) cz++;
+  
+	    vector<int> r;
+	    if (cy > a.size()/3) r.push_back(y);
+	    if (cz > a.size()/3) r.push_back(z);
+	    return r;
+    }
+};
+
+// TC = O(N)
+// SC = O(1)
